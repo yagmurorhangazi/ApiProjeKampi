8b85f650c21fec3ca6a18d4f95ae707941e9f795
yagmurorhangazi <ezgiyagmurorhangazi@gmail.com>
2026-05-20 17:08:49 +0300

    Add project files.

---
+++ ApiProjeKampi.WebApi/Entities/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace ApiProjeKampi.WebApi.Entities
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string  CatgeoryName { get; set; }
+     
+    }
+}
---
+++ ApiProjeKampi.WebApi/Entities/Services.cs
@@ -0,0 +1,11 @@
+﻿namespace ApiProjeKampi.WebApi.Entities
+{
+    public class Services
+    {
+
+        public int ServicesId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string IconUrl { get; set; }
+    }
+}
